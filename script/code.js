ffd25c3d6ee3e18048c0bb678810b4f5dd1a665b
mariiazhivonitko <mariia.zhivonitko@gmail.com>
2022-12-10 00:22:45 +0200

    check question1

---
+++ script/code.js
@@ -0,0 +1,10 @@
+//Check question1
+
+let answerInput1 = Number(document.getElementById("answer1").value);
+document.getElementById("check1").addEventListener("click", checkAnswer);
+
+function checkAnswer() {
+    if(answerInput1 == 11){
+        
+    }
+}
