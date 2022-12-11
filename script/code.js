5f82e9011b2933a521ccca6bc17a2f91c42f4f30
mariiazhivonitko <mariia.zhivonitko@gmail.com>
2022-12-11 11:11:06 +0200

    Add radio button to question4

--- script/code.js
+++ script/code.js
@@ -1,10 +1,12 @@
 //Check question1
-
+document.getElementById("check1").onclick = checkAnswer();
 let answerInput1 = Number(document.getElementById("answer1").value);
-document.getElementById("check1").addEventListener("click", checkAnswer);
 
 function checkAnswer() {
+   
     if(answerInput1 == 11){
-        
+        console.log(answerInput1);
+    }else{
+        console.log("wrong");
     }
 }
