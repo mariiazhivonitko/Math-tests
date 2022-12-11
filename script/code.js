a6b4d28d04684b5abadba6b8c6d68554dfc7982d
mariiazhivonitko <mariia.zhivonitko@gmail.com>
2022-12-11 11:43:15 +0200

    Add preventdefault to check question1

--- script/code.js
+++ script/code.js
@@ -1,9 +1,13 @@
 //Check question1
 document.getElementById("check1").onclick = checkAnswer();
-let answerInput1 = Number(document.getElementById("answer1").value);
+//let answerInput1 = Number(document.getElementById("answer1").value);
 
-function checkAnswer() {
-   
+/**
+ * @param {Event} e 
+ */
+function checkAnswer(e) {
+    e.preventDefault();
+    let answerInput1 = Number(document.getElementById("answer1").value);
     if(answerInput1 == 11){
         console.log(answerInput1);
     }else{
