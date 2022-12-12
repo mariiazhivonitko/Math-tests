db4f93f6ad15bba594e4542ce4436f7be674a96c
mariiazhivonitko <mariia.zhivonitko@gmail.com>
2022-12-12 09:51:46 +0200

    Add function for right answers

--- script/code.js
+++ script/code.js
@@ -2,6 +2,40 @@
 //document.getElementById("check1").onclick = checkAnswer();
 //let answerInput1 = Number(document.getElementById("answer1").value);
 
+/**
+ * Returns the chosen radio-button value
+ */
+ function checkRadioButton(name) {
+    let radioButtons = document.getElementsByName(name);
+    let radioButtonAnswer;
+
+    for (radio of radioButtons) {
+        if(radio.checked){
+            radioButtonAnswer = radio.value;
+        }
+        
+    }
+    return radioButtonAnswer;
+}   
+
+/**
+ * 
+ * Returns comment for right answer
+ */
+function rightAnswer() {
+    let comment = document.createElement("p");
+    let bonus = document.createElement("img");
+    comment.textContent = "Mahtava! Vasaus on oikein. Saat";
+    comment.style.color = "#369f39";
+    comment.style.fontSize = "2em";
+
+    //Add bonus star-picture
+    //bonus.style.scr = "assets/answers/Star.svg";
+    return comment;
+}
+
+
+
 function checkAnswer1() {
     let answerInput1 = Number(document.getElementById("answer1").value);
     let image1 = document.getElementById("flower_img");
@@ -36,15 +70,33 @@ function checkAnswer1() {
     }
 }
 
-function checkAnswer2() {
-    let radioButtons = document.getElementsByName("square");
-    let radioButtonAnswer;
 
-    for (radio of radioButtons) {
-        if(radio.checked){
-            radioButtonAnswer = radio.value;
-        }
-        
-    }
-    console.log(radioButtonAnswer);
+
+function checkAnswer2(){
+    let answerRadioButton = checkRadioButton("square");
+
+    let image2 = document.getElementById("square_img");
+    let answerBox2 = document.getElementById("answerBox2");
+    let question2 =document.getElementById("question2");
+
+    image2.style.src = "assets/answers/square_answer.svg";
+    answerBox2.style.display = "none";
+    
+    let comment2 = rightAnswer();
+    question2.appendChild(comment2);
+   
+
+
 }
+function checkAnswer4(){
+    let answer = checkRadioButton("tetris");
+
+    console.log(answer);
+
+
+}
+
+    //Check if the answer is right
+    //if (radioButtonAnswer == "D") {
+
+    
